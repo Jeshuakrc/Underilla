598f7556689c9920eeaa3ede5d18fa1d030d2a1f
Jeshua <jeshuakrc@gmail.com>
2023-03-04 17:50:14 -0600

    Initial setup

---
+++ src/main/java/com/dotkntrell/mc/terraformer/Terraformer.java
@@ -0,0 +1,17 @@
+package com.dotkntrell.mc.terraformer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Terraformer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
