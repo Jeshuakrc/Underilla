675c1743b9f642618c068aa282977c817886a0bc
Jeshua <jeshuakrc@gmail.com>
2023-06-03 17:55:52 -0600

    Early implementation

--- Underilla-Fabric/src/main/java/com/jkantrell/mc/underilla/fabric/Underilla.java
+++ Underilla-Fabric/src/main/java/com/jkantrell/mc/underilla/fabric/Underilla.java
@@ -1,6 +1,12 @@
 package com.jkantrell.mc.underilla.fabric;
 
-public class Underilla {
+import net.fabricmc.api.ModInitializer;
+
+public class Underilla implements ModInitializer {
 
 
+    @Override
+    public void onInitialize() {
+
+    }
 }
---
+++ Underilla-Fabric/src/main/java/com/jkantrell/mc/underilla/fabric/mixin/WorldPresetMixin.java
@@ -0,0 +1,8 @@
+package com.jkantrell.mc.underilla.fabric.mixin;
+
+import net.minecraft.world.gen.WorldPresets;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(targets = WorldPresets)
+public class WorldPresetMixin {
+}
