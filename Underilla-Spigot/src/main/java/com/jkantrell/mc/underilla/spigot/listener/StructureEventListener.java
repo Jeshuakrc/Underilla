dbdeefc7aebe3636fceea6c19f8c0051e7a0944c
Jeshua <jeshuakrc@gmail.com>
2023-06-04 21:38:29 -0600

    Spigot - New config to customize structure generation

--- Underilla-Spigot/src/main/java/com/jkantrell/mc/underilla/spigot/listener/StructureEventListener.java
+++ Underilla-Spigot/src/main/java/com/jkantrell/mc/underilla/spigot/listener/StructureEventListener.java
@@ -1,11 +1,9 @@
 package com.jkantrell.mc.underilla.spigot.listener;
 
-import com.jkantrell.mc.underilla.spigot.io.Config;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.AsyncStructureSpawnEvent;
 import org.bukkit.generator.structure.Structure;
-
 import java.util.List;
 
 public class StructureEventListener implements Listener {
